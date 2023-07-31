aa97ba30e00ebfef849b78b3f51c8bba4fa73526
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-07-31 09:13:15 -0300

    Added package Planet

---
+++ src/components/LoremIpsum.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const LoremIpsum = () => {
+    return <p>Eu espero que você aprenda muitooo react o/</p>
+}
+export default LoremIpsum;
---
+++ src/components/planets/planet/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const Planet = () => {
+    return (
+        <div>
+            <h4>Mercúrio</h4>
+            <p>Lorem Ipsum.... Lorem Ipsum.... Lorem Ipsum.... Lorem Ipsum....</p>
+            <img className="grayImg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"/>
+        </div>
+    );
+}
+export default Planet;
