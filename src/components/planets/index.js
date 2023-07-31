4fd7ce49676337ab48b006dfd01f6764318ef6de
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-07-31 12:34:19 -0300

    Implemented States With React

--- src/components/planets/index.js
+++ src/components/planets/index.js
@@ -5,23 +5,68 @@ const showMessage = () => {
     console.log('Evento!');
 }
 
-const Planets = () => {
-    return (
-        <Fragment>
-            <h3>Planet List</h3>
-            <button onClick={showMessage}>Botao</button>
-            <hr/>
-            <Planet name="Mercúrio" description="Mercúrio é o menor planeta do universo!"
-                    img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"
-                    link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
-                    gray={true}/>
-            <Planet name="Plutão"
-                    description="Plutão, formalmente designado 134340 Plutão (símbolos: ⯓ e ♇) é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol."
-                    img_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg"
-                    link="https://pt.wikipedia.org/wiki/Plut%C3%A3o"
-                    title_with_underline={true}
-                    gray={true}/>
-        </Fragment>
-    );
+class Planets extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            planets: [
+                {
+                    name: "Mercúrio",
+                    description: "Mercúrio é o menor planeta do universo!",
+                    img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg",
+                    link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
+                },
+                {
+                    name: "Plutão",
+                    description: "Plutão, formalmente designado 134340 Plutão (símbolos: ⯓ e ♇) é um planeta anão do Sistema Solar e o nono maior e décimo mais massivo objeto observado diretamente orbitando o Sol.",
+                    img_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Pluto_in_True_Color_-_High-Res.jpg/280px-Pluto_in_True_Color_-_High-Res.jpg",
+                    link: "https://pt.wikipedia.org/wiki/Plut%C3%A3o",
+                }
+            ]
+        }
+    }
+
+    removeLast = () => {
+        //Os 3 ... é um operador que extrai tudo!
+        let new_planets = [...this.state.planets]
+        new_planets.pop();
+        this.setState(state => ({
+            planets: new_planets
+        }))
+    }
+
+    duplicateLastPlanet = () => {
+        let last_planet = this.state.planets[this.state.planets.length - 1]
+        this.setState(state => ({
+            planets: [...this.state.planets, last_planet]
+        }))
+    }
+    render() {
+        return (
+            <Fragment>
+                <h3>Planet List</h3>
+                <button onClick={
+                    this.removeLast
+                }>Remove Last
+                </button>
+                <button onClick={
+                    this.duplicateLastPlanet
+                }>Duplicate Last
+                </button>
+                <hr/>
+                {this.state.planets.map((planet) =>
+                    <Planet
+                        name={planet.name}
+                        description={planet.description}
+                        img_url={planet.img_url}
+                        link={planet.link}
+                    />
+                )}
+
+            </Fragment>
+        );
+    }
 }
+
 export default Planets;
