348c0b628abc021adcdd6fbd059c24e9a8e09ae6
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-07-31 09:22:35 -0300

    Added Reutilzizaded Components

--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './style.css';
+import '../../shared/gray_img/style.css';
+import GrayImg from "../../shared/gray_img";
 
 const Planet = () => {
     return (
         <div>
             <h4>Mercúrio</h4>
             <p>Lorem Ipsum.... Lorem Ipsum.... Lorem Ipsum.... Lorem Ipsum....</p>
-            <img className="grayImg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"/>
+            <GrayImg/>
         </div>
     );
 }
---
+++ src/components/shared/gray_img/index.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react';
+import './style.css'
+
+const GrayImg = () => {
+    return (
+        <img className="grayImg"
+             src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/280px-Mercury_in_color_-_Prockter07-edit1.jpg"/>
+    );
+}
+export default GrayImg;
